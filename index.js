97f44257b611bddcf28ac4c89724dff14b3b5f2f
Caitlin Kinney <thecaitlinkinney@gmail.com>
2021-09-30 21:57:56 -0700

    Implements relatedProducts route

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
 const express = require('express');
 // const queries = require('./models/models.js');
 const db = require('./db/database.js')
-
 const app = express();
-
 const port = 5000;
-
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
+})
+
+app.get('/products/:product_id/related', (req, res) => {
+  var id = req.params.product_id
+  var params = [id];
+  var text = 'SELECT * FROM related WHERE current_product = $1'
+
+  db.query(text, params, (error, data) => {
+    if (error) {
+      res.set('status', 500).send(error)
+    } else {
+      Promise.resolve(
+        data.rows.map((row) => {
+        return row.related_product;
+      }))
+        .then((data) => res.set('status', 200).send(data))
+    }
+  })
 })
 
 app.get('/products/:product_id', (req, res) => {
@@ -42,11 +57,7 @@ app.get('/products', (req, res) => {
       res.set('status', 200).send(data.rows)
     }
   })
-
 })
-
-
-
 
 app.listen(port, () => {
   console.log(`app listening on port ${port}`)
