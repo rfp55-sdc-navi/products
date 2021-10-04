09e411fbd43abb23bc97dbdd1ce9b7d09c8b1138
Caitlin Kinney <thecaitlinkinney@gmail.com>
2021-10-04 15:06:08 -0700

    Adds ')' to random number generation (typo)

--- k6Tests/singleProductStylesEndpoint.js
+++ k6Tests/singleProductStylesEndpoint.js
@@ -2,7 +2,7 @@ import http from 'k6/http';
 import {sleep} from 'k6';
 
 export default function () {
-  var id = Math.floor(Math.random() * 100000
+  var id = Math.floor(Math.random() * 100000)
   http.get(`http://localhost:5000/products/${id}/styles`);
   sleep(1)
 }
